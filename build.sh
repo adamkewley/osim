acaf725c6570dcc3c5f64464e5f796d16407d554
Adam Kewley <contact@adamkewley.com>
2024-10-02 12:09:03 +0200

    Minor modification

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ OSIM_BUILD_TYPE=RelMinSize
 
 rm -rf build/ deps-build/ install/
 
-cmake -S third_party/ -B deps-build -DCMAKE_BUILD_TYPE=${OSIM_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${PWD}/install && cmake --build deps-build -j20 && cmake -S . -B build -DCMAKE_PREFIX_PATH=${PWD}/install -DCMAKE_BUILD_TYPE=${OSIM_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${PWD}/install && cmake --build build -j20
+cmake -S third_party/ -B deps-build -DCMAKE_BUILD_TYPE=${OSIM_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${PWD}/install && cmake --build deps-build -j20 && cmake -S . -B build -DCMAKE_PREFIX_PATH=${PWD}/install -DCMAKE_BUILD_TYPE=${OSIM_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${PWD}/install && cmake --build build -j20 --target install
