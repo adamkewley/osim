0b822af91f7ccd14cf339835330c9b56da12f1fe
Adam Kewley <contact@adamkewley.com>
2024-10-02 19:33:08 +0200

    Update buildscript with one that worked for osc

--- build.sh
+++ build.sh
@@ -1,7 +1,25 @@
-#export CFLAGS=-fPIC
-#export CXXFLAGS=-fPIC
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+export CC=clang
+export CXX=clang++
 OSIM_BUILD_TYPE=MinSizeRel
 
 rm -rf build/ deps-build/ install/
 
-cmake -S third_party/ -B deps-build -DCMAKE_BUILD_TYPE=${OSIM_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${PWD}/install && cmake --build deps-build -j20 && cmake -S . -B build -DCMAKE_PREFIX_PATH=${PWD}/install -DCMAKE_BUILD_TYPE=${OSIM_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${PWD}/install && cmake --build build -j20 --target install
+# config+build these deps into install/
+cmake -S third_party/ -B deps-build -DCMAKE_BUILD_TYPE=${OSIM_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${PWD}/install
+cmake --build deps-build -j20
+
+# config+build opensim deps into install/
+cmake -S ../opensim-creator/third_party -B oscdeps-build -DCMAKE_BUILD_TYPE=${OSIM_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${PWD}/install -DOSCDEPS_GET_OPENSIM=OFF
+cmake --build oscdeps-build -j20
+
+# config+build osim into install/
+cmake -S . -B build -DCMAKE_PREFIX_PATH=${PWD}/install -DCMAKE_BUILD_TYPE=${OSIM_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${PWD}/install
+cmake --build build -j20 --target install
+
+# config+build modified OpenSimCreator
+cmake -S ../opensim-creator/ -B osc-build/ -DCMAKE_BUILD_TYPE=${OSIM_BUILD_TYPE} -DCMAKE_PREFIX_PATH=${PWD}/install
+cmake --build osc-build -j20 -v
